1c82c4bb030fb4c0e34a4a7175d6f17a4b092a1a
Kai Yang <yk000123@sina.com>
2015-04-17 12:40:11 +0800

    Add functional test for password authentication

--- Kfstorm.DoubanFM.Core.FunctionalTest/Generator.cs
+++ Kfstorm.DoubanFM.Core.FunctionalTest/Generator.cs
@@ -11,6 +11,10 @@ namespace Kfstorm.DoubanFM.Core.FunctionalTest
         public static string AppName => ConfigurationManager.AppSettings["AppName"];
         public static string AppVersion => ConfigurationManager.AppSettings["AppVersion"];
         public static string Udid => Guid.NewGuid().ToString("N");
+        public static string Username => ConfigurationManager.AppSettings["Username"];
+        public static string MailAddress => ConfigurationManager.AppSettings["MailAddress"];
+        public static string Password => ConfigurationManager.AppSettings["Password"];
+
         public static IServerConnection ServerConnection => new ServerConnection(ClientId, ClientSecret, AppName, AppVersion, RedirectUri, Udid);
         public static ISession Session => new Session(ServerConnection);
         public static IPlayer Player => new Player(Session);
---
+++ Kfstorm.DoubanFM.Core.FunctionalTest/PasswordAuthenticationTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace Kfstorm.DoubanFM.Core.FunctionalTest
+{
+    [TestFixture]
+    public class PasswordAuthenticationTests
+    {
+        private static void ValidateUserInfo(UserInfo userInfo)
+        {
+            Assert.IsNotNull(userInfo);
+            Assert.IsNotEmpty(userInfo.AccessToken);
+            Assert.IsNotEmpty(userInfo.RefreshToken);
+            Assert.IsNotEmpty(userInfo.Username);
+            Assert.Greater(userInfo.ExpiresIn, 0);
+            Assert.Greater(userInfo.UserId, 0);
+        }
+
+        [Test]
+        public async void TestLogOnSuccess_MailAddress()
+        {
+            var authentication = new PasswordAuthentication(Generator.ServerConnection)
+            {
+                Username = Generator.MailAddress,
+                Password = Generator.Password
+            };
+            var userInfo = await authentication.Authenticate();
+            ValidateUserInfo(userInfo);
+        }
+
+        [Test]
+        public async void TestLogOnSuccess_UpperCaseMailAddress()
+        {
+            var authentication = new PasswordAuthentication(Generator.ServerConnection)
+            {
+                Username = Generator.MailAddress.ToUpper(),
+                Password = Generator.Password
+            };
+            var userInfo = await authentication.Authenticate();
+            ValidateUserInfo(userInfo);
+        }
+
+        [Test]
+        public async void TestLogOnSuccess_Username()
+        {
+            var authentication = new PasswordAuthentication(Generator.ServerConnection)
+            {
+                Username = Generator.Username,
+                Password = Generator.Password
+            };
+            var userInfo = await authentication.Authenticate();
+            ValidateUserInfo(userInfo);
+        }
+    }
+}
